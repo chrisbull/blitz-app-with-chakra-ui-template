e488647f51425a2a530125cae0266f7d5bdd90f8
Chris Bull <chris@chrisbull.com>
2021-02-01 10:30:56 -0800

    add next-seo

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,5 +1,5 @@
+import { NextSeo } from 'next-seo'
 import { ReactNode } from 'react'
-import { Head } from 'blitz'
 
 type LayoutProps = {
   title?: string
@@ -9,16 +9,7 @@ type LayoutProps = {
 const Layout = ({ title, children }: LayoutProps) => {
   return (
     <>
-      <Head>
-        <title>{title || 'blitz-app-with-chakra-ui-template'}</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
+      <NextSeo title={title} />
       {children}
     </>
   )
