77d13ff99fb7e69ea95fd50a45d67f7a73698ea4
Chris Bull <chris@chrisbull.com>
2021-01-31 18:21:22 -0800

    add RememberMe checkbox to login page

--- app/auth/components/LoginForm.tsx
+++ app/auth/components/LoginForm.tsx
@@ -4,6 +4,7 @@ import { LabeledTextField } from "app/core/components/LabeledTextField"
 import { Form, FORM_ERROR } from "app/core/components/Form"
 import login from "app/auth/mutations/login"
 import { Login } from "app/auth/validations"
+import { Checkbox } from "@chakra-ui/react"
 
 type LoginFormProps = {
   onSuccess?: () => void
@@ -38,6 +39,7 @@ export const LoginForm = (props: LoginFormProps) => {
       >
         <LabeledTextField name="email" label="Email" placeholder="Email" />
         <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
+        <Checkbox>Remember Me</Checkbox>
         <div>
           <Link href="/forgot-password">
             <a>Forgot your password?</a>
