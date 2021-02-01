b44fef63ed44b6b5e1f63bf107bdf8a09dd63aeb
Chris Bull <chris@chrisbull.com>
2021-01-31 19:18:51 -0800

    updates to login and sign up page styling

--- app/auth/components/LoginForm.tsx
+++ app/auth/components/LoginForm.tsx
@@ -4,7 +4,7 @@ import { LabeledTextField } from "app/core/components/LabeledTextField"
 import { Form, FORM_ERROR } from "app/core/components/Forms/Form"
 import login from "app/auth/mutations/login"
 import { Login } from "app/auth/validations"
-import { Box, Button, Checkbox, Container, Heading, VStack } from "@chakra-ui/react"
+import { Box, Button, Checkbox, Container, Flex, Heading, Text, VStack } from "@chakra-ui/react"
 
 type LoginFormProps = {
   onSuccess?: () => void
@@ -14,50 +14,57 @@ export const LoginForm = (props: LoginFormProps) => {
   const [loginMutation] = useMutation(login)
 
   return (
-    <Container>
-      <Box p={10} shadow="md" borderRadius="md">
-        <Heading size="lg">Login</Heading>
+    <Flex bg="gray.500" minH="100vh" justify="center" align="center">
+      <Container>
+        <Box p={10} shadow="md" borderRadius="md" bg="white">
+          <Heading size="lg">Login</Heading>
 
-        <Form
-          submitText="Login"
-          schema={Login}
-          initialValues={{ email: "", password: "" }}
-          onSubmit={async (values) => {
-            try {
-              await loginMutation(values)
-              props.onSuccess?.()
-            } catch (error) {
-              if (error instanceof AuthenticationError) {
-                return { [FORM_ERROR]: "Sorry, those credentials are invalid" }
-              } else {
-                return {
-                  [FORM_ERROR]:
-                    "Sorry, we had an unexpected error. Please try again. - " + error.toString(),
+          <Form
+            submitText="Login"
+            schema={Login}
+            initialValues={{ email: "", password: "" }}
+            onSubmit={async (values) => {
+              try {
+                await loginMutation(values)
+                props.onSuccess?.()
+              } catch (error) {
+                if (error instanceof AuthenticationError) {
+                  return { [FORM_ERROR]: "Sorry, those credentials are invalid" }
+                } else {
+                  return {
+                    [FORM_ERROR]:
+                      "Sorry, we had an unexpected error. Please try again. - " + error.toString(),
+                  }
                 }
               }
-            }
-          }}
-        >
-          <LabeledTextField name="email" label="Email" placeholder="Email" />
-          <LabeledTextField
-            name="password"
-            label="Password"
-            placeholder="Password"
-            type="password"
-          />
-          <Checkbox>Remember Me</Checkbox>
-          <Box>
-            <Link href="/forgot-password">
-              <Button variant="link">Forgot your password?</Button>
-            </Link>
-          </Box>
-        </Form>
+            }}
+          >
+            <LabeledTextField name="email" label="Email" placeholder="Email" />
+            <LabeledTextField
+              name="password"
+              label="Password"
+              placeholder="Password"
+              type="password"
+            />
+            <Checkbox>Remember Me</Checkbox>
+            <Box>
+              <Link href="/forgot-password">
+                <Button variant="link">Forgot your password?</Button>
+              </Link>
+            </Box>
+          </Form>
 
-        <Box mt={5}>
-          Or <Link href="/signup">Sign Up</Link>
+          <Box mt={5} textAlign="center" borderTop="1px solid" borderTopColor="gray.100" pt={5}>
+            <Text>
+              Don't have an account?{" "}
+              <Link href="/signup">
+                <Button variant="link">Sign Up</Button>
+              </Link>
+            </Text>
+          </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+    </Flex>
   )
 }
 
--- app/auth/components/SignupForm.tsx
+++ app/auth/components/SignupForm.tsx
@@ -1,10 +1,10 @@
 import React from "react"
-import { useMutation } from "blitz"
+import { Link, useMutation } from "blitz"
 import { LabeledTextField } from "app/core/components/LabeledTextField"
 import { Form, FORM_ERROR } from "app/core/components/Forms/Form"
 import signup from "app/auth/mutations/signup"
 import { Signup } from "app/auth/validations"
-import { Box, Container, Heading } from "@chakra-ui/react"
+import { Box, Button, Container, Flex, Heading, Text } from "@chakra-ui/react"
 
 type SignupFormProps = {
   onSuccess?: () => void
@@ -14,38 +14,48 @@ export const SignupForm = (props: SignupFormProps) => {
   const [signupMutation] = useMutation(signup)
 
   return (
-    <Container>
-      <Box p={10} shadow="md" borderRadius="md">
-        <Heading size="lg">Create an Account</Heading>
+    <Flex bg="gray.500" minH="100vh" justify="center" align="center">
+      <Container>
+        <Box p={10} shadow="md" borderRadius="md" bg="white">
+          <Heading size="lg">Create an Account</Heading>
 
-        <Form
-          submitText="Create Account"
-          schema={Signup}
-          initialValues={{ email: "", password: "" }}
-          onSubmit={async (values) => {
-            try {
-              await signupMutation(values)
-              props.onSuccess?.()
-            } catch (error) {
-              if (error.code === "P2002" && error.meta?.target?.includes("email")) {
-                // This error comes from Prisma
-                return { email: "This email is already being used" }
-              } else {
-                return { [FORM_ERROR]: error.toString() }
+          <Form
+            submitText="Create Account"
+            schema={Signup}
+            initialValues={{ email: "", password: "" }}
+            onSubmit={async (values) => {
+              try {
+                await signupMutation(values)
+                props.onSuccess?.()
+              } catch (error) {
+                if (error.code === "P2002" && error.meta?.target?.includes("email")) {
+                  // This error comes from Prisma
+                  return { email: "This email is already being used" }
+                } else {
+                  return { [FORM_ERROR]: error.toString() }
+                }
               }
-            }
-          }}
-        >
-          <LabeledTextField name="email" label="Email" placeholder="Email" />
-          <LabeledTextField
-            name="password"
-            label="Password"
-            placeholder="Password"
-            type="password"
-          />
-        </Form>
-      </Box>
-    </Container>
+            }}
+          >
+            <LabeledTextField name="email" label="Email" placeholder="Email" />
+            <LabeledTextField
+              name="password"
+              label="Password"
+              placeholder="Password"
+              type="password"
+            />
+          </Form>
+          <Box mt={5} textAlign="center" borderTop="1px solid" borderTopColor="gray.100" pt={5}>
+            <Text>
+              Already have an account?{" "}
+              <Link href="/login">
+                <Button variant="link">Login</Button>
+              </Link>
+            </Text>
+          </Box>
+        </Box>
+      </Container>
+    </Flex>
   )
 }
 
