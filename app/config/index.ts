fbf30cda9739c93be189d81c0dc7ec4e9aaadaa4
Chris Bull <chris@chrisbull.com>
2021-02-01 10:34:11 -0800

    fix issue with missing variable

--- app/config/index.ts
+++ app/config/index.ts
@@ -10,20 +10,3 @@ export const {
 export const DEV_SEND_EMAILS = Boolean(process.env.DEV_SEND_EMAILS)
 
 export const IS_PRODUCTION = APP_ENV === 'production'
-
-export const DEV_EMAIL_OPTIONS: any = {
-  host: 'localhost',
-  port: 1025,
-  secure: false,
-  debug: true,
-  ignoreTLS: true,
-}
-
-export const S3_CONFIG = {
-  signatureVersion: 'v4',
-  region: 'us-west-1',
-}
-
-export const S3_URL = `https://${AWS_S3_BUCKET}.s3.amazonaws.com/`
-
-export const FULL_WEB_URL = `${IS_PRODUCTION ? 'https://' : 'http://'}${WEB_URL}`
